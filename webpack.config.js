35c1891a9cd5165c60bb5fb945f91a630ed3025c
Preston Gramberg <knighttraintint@gmail.com>
2019-10-10 11:40:43 -0500

    Added webpack versioning

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    .enableVersioning()
 
     // enables @babel/preset-env polyfills
     .configureBabel(() => {}, {
