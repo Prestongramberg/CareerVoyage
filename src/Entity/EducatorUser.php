ad9fdb91e8b86d66cd3a829dc24ef20ff94839ed
Preston Gramberg <knighttraintint@gmail.com>
2019-12-14 15:21:11 -0600

    fixed impersonations

--- src/Entity/EducatorUser.php
+++ src/Entity/EducatorUser.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
@@ -327,4 +328,5 @@ class EducatorUser extends User
 
         return $this;
     }
+
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -1100,4 +1101,5 @@ abstract class User implements UserInterface
     {
         $this->temporarySecurityToken = bin2hex(random_bytes(32));
     }
+
 }
