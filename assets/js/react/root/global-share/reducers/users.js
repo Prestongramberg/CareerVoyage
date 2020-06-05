35e56360edfc4efd94c99b1a347f54bacd7ae1fb
Preston Gramberg <knighttraintint@gmail.com>
2020-06-05 04:56:23 +0000

    Share Fixes

--- assets/js/react/root/global-share/reducers/users.js
+++ assets/js/react/root/global-share/reducers/users.js
@@ -5,7 +5,7 @@ export default (state = {}, action) => {
     switch (action.type) {
 
         case actionTypes.SEARCH_CHATTABLE_USERS_SUCCESS:
-            return action.users;
+            return action.users.sort((a, b) => ( (a.first_name.toUpperCase() + a.last_name.toUpperCase()) > (b.first_name.toUpperCase() + b.last_name.toUpperCase())) ? 1 : -1);
 
         case actionTypes.NOTIFICATIONS_SENDING_SUCCESS:
             return []
