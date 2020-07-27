dac8ac9256a89213ff6fde0f1d902411182af775
Preston Gramberg <knighttraintint@gmail.com>
2020-07-27 11:57:02 -0500

    updated schools edit on professional page

--- assets/js/Custom.js
+++ assets/js/Custom.js
@@ -951,4 +951,14 @@ jQuery(document).ready(function($) {
         });
     });
 
+
+    /**
+     * Load tab if "page" is set
+     */
+    
+    if(window.location.hash) {
+        document.getElementById( window.location.hash.substr(1) ).click();
+        window.scrollTo(0,0);
+    }
+
 });
