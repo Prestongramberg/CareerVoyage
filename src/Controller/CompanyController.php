91ce77a72b1d2b85c312e27c796090ea5ca17706
Preston Gramberg <knighttraintint@gmail.com>
2020-08-05 10:06:01 -0500

    updated send notification to student when creating / updating event

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -904,24 +904,36 @@ class CompanyController extends AbstractController
 
             $this->entityManager->flush();
 
-            // Send email to students that are interested in the event that was created
-            $items = $experience->getSecondaryIndustries();
-            $loggedInUser = $this->getUser();
             
-            $chosen_students = [];
-            foreach($items as $k => $v) {
-                $students = $this->studentUserRepository->findStudentBySecondaryIndustry(intval($v->getId()));
-                foreach($students as $student){
-                    $chosen_students[] = $student;
+            if($_POST['notify_students'] == "match") {
+                // Send email to students that are interested in the event that was created
+                $items = $experience->getSecondaryIndustries();
+                $loggedInUser = $this->getUser();
+                
+                $chosen_students = [];
+                foreach($items as $k => $v) {
+                    $students = $this->studentUserRepository->findStudentBySecondaryIndustry(intval($v->getId()));
+                    foreach($students as $student){
+                        $chosen_students[] = $student;
+                    }
+                }
+            }
+            if($_POST['notify_students'] == "all") {
+                $chosen_students = [];
+                $students = $this->studentUserRepository->findAll();
+                foreach($students as $k => $v) {
+                    $chosen_students[] = $v;
                 }
             }
 
-            $message = $request->get('message', '');
-            $message = sprintf("Event: %s Message: %s", $experience->getTitle(), $message);
+            if($_POST['notify_students'] != "none") {
+                $message = $request->get('message', '');
+                $message = sprintf("Event: %s Message: %s", $experience->getTitle(), $message);
 
-            foreach($chosen_students as $student) {
-                $s = $this->studentUserRepository->find($student);
-                $this->experienceMailer->experienceForwardToStudent($experience, $s, $message, $loggedInUser);
+                foreach($chosen_students as $student) {
+                    $s = $this->studentUserRepository->find($student);
+                    $this->experienceMailer->experienceForwardToStudent($experience, $s, $message, $loggedInUser);
+                }
             }
 
             $this->addFlash('success', 'Experience successfully created!');
@@ -929,11 +941,32 @@ class CompanyController extends AbstractController
             return $this->redirectToRoute('company_experience_view', ['id' => $experience->getId()]);
         }
 
-        return $this->render('company/new_experience.html.twig', [
-            'company' => $company,
-            'form' => $form->createView(),
-            'user' => $user,
-        ]);
+
+        if($_POST && $_POST['new_company_experience'] && $_POST['new_company_experience']['secondaryIndustries']) {
+            
+            $secondary_industries = [];
+            foreach($_POST['new_company_experience']['secondaryIndustries'] as $secondary) {
+
+                $secondaryIndustry = $this->secondaryIndustryRepository->find($secondary);
+
+                array_push($secondary_industries, array("id" => intval($secondary), "name" => $secondaryIndustry->getName(), "url" => $secondaryIndustry->getUrl()));
+            }
+            
+            
+            return $this->render('company/new_experience.html.twig', [
+                'company' => $company,
+                'form' => $form->createView(),
+                'user' => $user,
+                'secondaryIndustries' => $secondary_industries
+            ]);
+        } else {
+            return $this->render('company/new_experience.html.twig', [
+                'company' => $company,
+                'form' => $form->createView(),
+                'user' => $user,
+                'secondaryIndustries' => null
+            ]); 
+        }
     }
 
     /**
@@ -970,6 +1003,39 @@ class CompanyController extends AbstractController
             $this->entityManager->persist($experience);
             $experience->setCompany($company);
             $this->entityManager->flush();
+
+            if($_POST['notify_students'] == "match") {
+                // Send email to students that are interested in the event that was created
+                $items = $experience->getSecondaryIndustries();
+                $loggedInUser = $this->getUser();
+                
+                $chosen_students = [];
+                foreach($items as $k => $v) {
+                    $students = $this->studentUserRepository->findStudentBySecondaryIndustry(intval($v->getId()));
+                    foreach($students as $student){
+                        $chosen_students[] = $student;
+                    }
+                }
+            }
+            if($_POST['notify_students'] == "all") {
+                $chosen_students = [];
+                $students = $this->studentUserRepository->findAll();
+                foreach($students as $k => $v) {
+                    $chosen_students[] = $v;
+                }
+            }
+
+            if($_POST['notify_students'] != "none") {
+                $message = $request->get('message', '');
+                $message = sprintf("Event: %s Message: %s", $experience->getTitle(), $message);
+
+                foreach($chosen_students as $student) {
+                    $s = $this->studentUserRepository->find($student);
+                    $this->experienceMailer->experienceForwardToStudent($experience, $s, $message, $loggedInUser);
+                }
+            }
+
+
             $this->addFlash('success', 'Experience successfully updated!');
 
             return $this->redirectToRoute('company_experience_edit', ['id' => $experience->getId()]);
