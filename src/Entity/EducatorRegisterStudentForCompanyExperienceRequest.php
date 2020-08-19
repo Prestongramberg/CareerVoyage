bedef31a31a99e64e19f6e14a5dd80602c7d9744
Preston Gramberg <knighttraintint@gmail.com>
2020-08-19 11:00:04 -0500

    udpated request display for users

--- src/Entity/EducatorRegisterStudentForCompanyExperienceRequest.php
+++ src/Entity/EducatorRegisterStudentForCompanyExperienceRequest.php
@@ -30,6 +30,16 @@ class EducatorRegisterStudentForCompanyExperienceRequest extends Request
      */
     protected $studentHasSeen = false;
 
+    /**
+     * @ORM\Column(type="boolean", nullable=false, options={"default":true})
+     */
+    protected $educatorHasSeen = false;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=false, options={"default":true})
+     */
+    protected $professionalHasSeen = false;
+
     public function getCompanyExperience(): ?CompanyExperience
     {
         return $this->companyExperience;
@@ -62,6 +72,28 @@ class EducatorRegisterStudentForCompanyExperienceRequest extends Request
     public function setStudentHasSeen(?bool $studentHasSeen): self
     {
         $this->studentHasSeen = $studentHasSeen;
+        return $this;
+    }
+
+    public function getEducatorHasSeen(): ?bool
+    {
+        return $this->educatorHasSeen;
+    }
+
+    public function setEducatorHasSeen(?bool $educatorHasSeen): self
+    {
+        $this->educatorHasSeen = $educatorHasSeen;
+        return $this;
+    }
+
+    public function getProfessionalHasSeen(): ?bool
+    {
+        return $this->professionalHasSeen;
+    }
+
+    public function setProfessionalHasSeen(?bool $professionalHasSeen): self
+    {
+        $this->professionalHasSeen = $professionalHasSeen;
         return $this;
     }
 }
