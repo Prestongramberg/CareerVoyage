79ecd9012f4c89c47b98be6f713cbc11b928c4a9
Preston Gramberg <knighttraintint@gmail.com>
2020-08-28 13:26:14 -0500

    updated company listing layout for mobile views

--- assets/js/react/components/CompanyListing/CompanyListing.js
+++ assets/js/react/components/CompanyListing/CompanyListing.js
@@ -10,8 +10,8 @@ class CompanyListing extends Component {
 
     render() {
         return (
-            <div className="uk-card uk-card-default uk-grid-collapse uk-flex-center uk-margin" data-uk-grid>
-                <div className="uk-card-media-left uk-width-1-1 uk-width-medium@m">
+            <div className="uk-card uk-card-default uk-grid-collapse uk-flex-left uk-margin" data-uk-grid>
+                <div className="uk-card-media-left uk-width-1-1 uk-width-medium@m uk-visible@m">
                     <div className="company-listing__image uk-height-1-1 uk-flex uk-flex-right uk-flex-bottom uk-background-cover uk-light"
                         style={{backgroundImage: `url(${this.props.image})`, minHeight: '150px'}}>
                         <div className="uk-inline uk-padding-small">
@@ -25,17 +25,53 @@ class CompanyListing extends Component {
                         </div>
                     </div>
                 </div>
+
+
+                <div className="uk-grid-collapse uk-flex-left uk-hidden@m" data-uk-grid>
+                    <div className="uk-card-media-left uk-width-auto">
+                        <div className="company-listing__image uk-width-1-1 uk-width-small uk-height-small uk-height-max-small uk-flex uk-flex-right uk-flex-bottom uk-background-cover uk-light"
+                            style={{backgroundImage: `url(${this.props.image})`, height: '150px'}}>
+                            <div className="uk-inline uk-padding-small">
+                                <FavoriteCompany
+                                    boundByProps={true}
+                                    id={this.props.id}
+                                    isFavorited={this.props.isFavorite}
+                                    companyFavorited={this.props.companyFavorited}
+                                    companyUnfavorited={this.props.companyUnfavorited}
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="uk-width-expand">
+                        <div className="uk-card-body">
+                            <a href={ window.Routing.generate('company_view', { id: this.props.id }) }>
+                                <h4 className="uk-card-title-small">{ this.props.name }</h4>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="uk-width-1-1 uk-width-expand@m">
                     <div className="uk-card-body">
                         <div className="company-listing__meta">
-                            <a href={ window.Routing.generate('company_view', { id: this.props.id }) }>
-                                <h4 className="uk-card-title-small uk-heading-divider">{ this.props.name }</h4>
-                            </a>
-                            <p>
-                                { this.props.primaryIndustry && <strong> {this.props.primaryIndustry.name } </strong> }
-                                <br />
-                                { this.props.description }
-                            </p>
+                            <div className="uk-hidden@m">
+                                <p style={{borderTop: '1px solid #e5e5e5', paddingTop: '10px'}}>
+                                    { this.props.primaryIndustry && <strong> {this.props.primaryIndustry.name } </strong> }
+                                    <br />
+                                    { this.props.description }
+                                </p>
+                            </div>
+                            <div className="uk-visible@m">
+                                <a href={ window.Routing.generate('company_view', { id: this.props.id }) }>
+                                    <h4 className="uk-card-title-small uk-heading-divider">{ this.props.name }</h4>
+                                </a>
+                                <p>
+                                    { this.props.primaryIndustry && <strong> {this.props.primaryIndustry.name } </strong> }
+                                    <br />
+                                    { this.props.description }
+                                </p>
+                            </div>
                             <div className="uk-grid uk-flex-middle" data-uk-grid>
                                 <div className="uk-width-auto">
                                     <div className="company-links">
