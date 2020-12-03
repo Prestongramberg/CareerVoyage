a22501352e3d7bf1ab720572bd24f6101de0927a
Preston Gramberg <knighttraintint@gmail.com>
2020-12-03 01:04:14 -0600

    bug fixes

---
+++ src/Repository/SystemUserRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SystemUser;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method SystemUser|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SystemUser|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SystemUser[]    findAll()
+ * @method SystemUser[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SystemUserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, SystemUser::class);
+    }
+
+    // /**
+    //  * @return SystemUser[] Returns an array of SystemUser objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?SystemUser
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+
+    /**
+     * @param $search
+     * @return mixed[]
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function findBySearchTerm($search) {
+
+        $query = sprintf('SELECT u.id, u.first_name, u.last_name, "SYSTEM_USER" as role, CONCAT("/media/cache/squared_thumbnail_small/uploads/profile_photo/", u.photo) as photoImageURL from user u inner join system_user su on u.id = su.id where CONCAT(u.first_name, " ", u.last_name) LIKE "%%%s%%"', $search);
+
+        $em = $this->getEntityManager();
+        $stmt = $em->getConnection()->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+}
--- src/Service/GlobalShare.php
+++ src/Service/GlobalShare.php
@@ -53,39 +53,29 @@ class GlobalShare
     private $stateCoordinatorRepository;
 
     /**
-     * @var ChatHelper
-     */
-    private $chatHelper;
-
-    /**
      * GlobalShare constructor.
-     * @param ProfessionalUserRepository $professionalUserRepository
-     * @param UserRepository $userRepository
-     * @param StudentUserRepository $studentUserRepository
-     * @param EducatorUserRepository $educatorUserRepository
-     * @param SiteAdminUserRepository $siteAdminRepository
+     *
+     * @param ProfessionalUserRepository    $professionalUserRepository
+     * @param UserRepository                $userRepository
+     * @param StudentUserRepository         $studentUserRepository
+     * @param EducatorUserRepository        $educatorUserRepository
+     * @param SiteAdminUserRepository       $siteAdminRepository
      * @param SchoolAdministratorRepository $schoolAdministratorRepository
-     * @param StateCoordinatorRepository $stateCoordinatorRepository
-     * @param ChatHelper $chatHelper
+     * @param StateCoordinatorRepository    $stateCoordinatorRepository
      */
     public function __construct(
-        ProfessionalUserRepository $professionalUserRepository,
-        UserRepository $userRepository,
-        StudentUserRepository $studentUserRepository,
-        EducatorUserRepository $educatorUserRepository,
-        SiteAdminUserRepository $siteAdminRepository,
-        SchoolAdministratorRepository $schoolAdministratorRepository,
-        StateCoordinatorRepository $stateCoordinatorRepository,
-        ChatHelper $chatHelper
+        ProfessionalUserRepository $professionalUserRepository, UserRepository $userRepository,
+        StudentUserRepository $studentUserRepository, EducatorUserRepository $educatorUserRepository,
+        SiteAdminUserRepository $siteAdminRepository, SchoolAdministratorRepository $schoolAdministratorRepository,
+        StateCoordinatorRepository $stateCoordinatorRepository
     ) {
-        $this->professionalUserRepository = $professionalUserRepository;
-        $this->userRepository = $userRepository;
-        $this->studentUserRepository = $studentUserRepository;
-        $this->educatorUserRepository = $educatorUserRepository;
-        $this->siteAdminRepository = $siteAdminRepository;
+        $this->professionalUserRepository    = $professionalUserRepository;
+        $this->userRepository                = $userRepository;
+        $this->studentUserRepository         = $studentUserRepository;
+        $this->educatorUserRepository        = $educatorUserRepository;
+        $this->siteAdminRepository           = $siteAdminRepository;
         $this->schoolAdministratorRepository = $schoolAdministratorRepository;
-        $this->stateCoordinatorRepository = $stateCoordinatorRepository;
-        $this->chatHelper = $chatHelper;
+        $this->stateCoordinatorRepository    = $stateCoordinatorRepository;
     }
 
     /**
@@ -104,7 +94,7 @@ class GlobalShare
             'all'           => []
         ];
 
-        $chattableUsers = $this->chatHelper->getChattableUsers($loggedInUser, $search);
+        $chattableUsers = $this->getShareableUsers($loggedInUser, $search);
 
         $chattableUserIds = array_map(function($chattableUser) {
             return $chattableUser['id'];
@@ -209,5 +199,93 @@ class GlobalShare
 
         return $payload;
     }
+
+    /**
+     * @param User $loggedInUser
+     * @param string $search
+     * @return array
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getShareableUsers(User $loggedInUser, $search = '') {
+
+        $users = [];
+
+        /**
+         * Students can message
+         * 1. Educators that are part of the same school
+         * 2. School administrators that are part of the same school
+         * 3. Students that are part of the same school
+         * @var StudentUser $loggedInUser
+         */
+        if($loggedInUser->isStudent()) {
+            $educatorUsers = $this->educatorUserRepository->findBySearchTermAndSchool($search, $loggedInUser->getSchool());
+            $schoolAdministrators = $this->schoolAdministratorRepository->findBySearchTermAndSchool($search, $loggedInUser->getSchool());
+            // for now we are disabling student to student communication
+            //$studentUsers = $this->studentUserRepository->findBySearchTermAndSchool($search, $loggedInUser->getSchool());
+            $professionalUsers = $this->professionalUserRepository->findByAllowedCommunication($search, $loggedInUser);
+            $users = array_merge($professionalUsers, $educatorUsers, $schoolAdministrators);
+        }
+
+        /**
+         * Educators can message
+         * 1. Educators that are part of the same school
+         * 2. School administrators that are part of the same school
+         * 3. Students that are part of the same school
+         * 4. All Professional Users
+         * @var EducatorUser $loggedInUser
+         */
+        if($loggedInUser->isEducator()) {
+            $educatorUsers = $this->educatorUserRepository->findBySearchTermAndSchool($search, $loggedInUser->getSchool());
+            $schoolAdministrators = $this->schoolAdministratorRepository->findBySearchTermAndSchool($search, $loggedInUser->getSchool());
+            $studentUsers = $this->studentUserRepository->findBySearchTermAndSchool($search, $loggedInUser->getSchool());
+            $professionalUsers = $this->professionalUserRepository->findBySearchTerm($search);
+            $users = array_merge($educatorUsers, $schoolAdministrators, $studentUsers, $professionalUsers);
+        }
+
+        /**
+         * Professionals can message
+         * 1. All educators on the platform
+         * 2. All school administrators
+         * 4. All Professional Users
+         * @var ProfessionalUser $loggedInUser
+         */
+        if($loggedInUser->isProfessional()) {
+            $educatorUsers = $this->educatorUserRepository->findBySearchTerm($search);
+            $schoolAdministrators = $this->schoolAdministratorRepository->findBySearchTerm($search);
+            $professionalUsers = $this->professionalUserRepository->findBySearchTerm($search);
+
+            $studentUsers = [];
+            foreach($loggedInUser->getSchools() as $school) {
+                $studentUsers = array_merge($studentUsers, $this->studentUserRepository->findBySearchTermAndSchool($search, $school));
+            }
+
+            $users = array_merge($studentUsers, $educatorUsers, $schoolAdministrators, $professionalUsers);
+        }
+
+        /**
+         * School Administrators can message
+         * 1. All educators on the platform
+         * 2. All school administrators
+         * 4. All Professional Users
+         * @var SchoolAdministrator $loggedInUser
+         */
+        if($loggedInUser->isSchoolAdministrator()) {
+
+            // find users just for their school
+            foreach($loggedInUser->getSchools() as $school) {
+                $educatorUsers = $this->educatorUserRepository->findBySearchTermAndSchool($search, $school);
+                $schoolAdministrators = $this->schoolAdministratorRepository->findBySearchTerm($search);
+                $studentUsers = $this->studentUserRepository->findBySearchTermAndSchool($search, $school);
+                $users = array_merge($users, $studentUsers, $educatorUsers, $schoolAdministrators);
+            }
+
+            $professionalUsers = $this->professionalUserRepository->findBySearchTerm($search);
+
+            $users = array_merge($users, $professionalUsers);
+        }
+
+        return $users;
+    }
+
 
 }
