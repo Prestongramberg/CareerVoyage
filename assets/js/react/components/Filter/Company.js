242970def446463d1b7b6fbe5184acd474971914
Preston Gramberg <knighttraintint@gmail.com>
2021-01-13 02:19:12 -0600

    fixes

--- assets/js/react/components/Filter/Company.js
+++ assets/js/react/components/Filter/Company.js
@@ -4,21 +4,31 @@ import { truncate } from "../../utilities/string-utils";
 
 class Company extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            items: [
+                { id: 0, label: "item 1" },
+                { id: 2, label: "item 2", disabled: true },
+                { id: 3, label: "item 3", disabled: false },
+                { id: 4, label: "item 4" }
+            ],
+            selectedItems: []
+        };
+    }
+
+    handleChange(selectedItems) {
+        this.setState({ selectedItems });
+    }
     render() {
+        const { items, selectedItems } = this.state;
         return (
-            <div className="uk-width-1-1 uk-width-1-2@s uk-width-1-3@l">
-                <div className="uk-width-1-1 uk-text-truncate" data-uk-form-custom="target: > * > span:first-child">
-                    <select onChange={this.props.updateCompanyQuery}>
-                        <option value="">Filter by Company...</option>
-                        { this.props.companies.map( company => <option key={company.id} value={company.id}>{company.name}</option> ) }
-                    </select>
-                    <button className="uk-button uk-button-default uk-width-1-1 uk-width-autom@l" type="button"
-                            tabIndex="-1">
-                        <span></span>
-                        <span data-uk-icon="icon: chevron-down"></span>
-                    </button>
-                </div>
-            </div>
+            <MultiSelect
+                items={items}
+                selectedItems={selectedItems}
+                onChange={this.handleChange}
+            />
         );
     }
 }
