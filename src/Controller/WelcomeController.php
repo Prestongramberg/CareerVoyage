9ae7ab7450a6c842a1e74e68ec464c0885401314
Preston Gramberg <knighttraintint@gmail.com>
2022-06-23 13:27:50 -0500

    fix

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -45,7 +45,7 @@ class WelcomeController extends AbstractController
     public function index(Request $request, AuthenticationUtils $authenticationUtils, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator, string $uploadsPath)
     {
 
-        return $this->redirect('http://futureforward.org/');
+ //       return $this->redirect('http://futureforward.org/');
 
         $securityContext = $this->container->get('security.authorization_checker');
         if ($securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
